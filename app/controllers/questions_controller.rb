5332df9a6bad3222f428db05f2c02ce5b2fff421
cd ~/code/$deyvisong/dotfiles && zsh git_setup.sh <deyvisonalves@protonmail.com>
2023-08-14 15:15:37 +0200

    Stupid Coaching

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,15 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @answer = params[:question]
+    if @answer.downcase == 'I am going to work'.downcase
+      @response ='Great!'
+    elsif @answer.end_with?('?')
+      @response = 'Silly question, get dressed and go to work!'
+    else
+      @response = "I don't care, get dressed and go to work!"
+    end
+  end
+end
